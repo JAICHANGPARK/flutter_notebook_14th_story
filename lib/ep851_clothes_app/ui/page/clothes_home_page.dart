c18463dd72888190b266b0afb7a2997dc9bd96ad
Dreamwalker <aristojeff@gmail.com>
2021-07-15 21:33:31 +0900

    Create clothes_home_page.dart

---
+++ lib/ep851_clothes_app/ui/page/clothes_home_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ClothesHomePage extends StatelessWidget {
+  const ClothesHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
