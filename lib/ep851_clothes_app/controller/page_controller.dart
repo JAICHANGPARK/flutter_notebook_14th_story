dd202cf4e34253cc91d2f9e27d90085c889be91b
Dreamwalker <aristojeff@gmail.com>
2021-07-15 21:59:02 +0900

    Update page_controller.dart

--- lib/ep851_clothes_app/controller/page_controller.dart
+++ lib/ep851_clothes_app/controller/page_controller.dart
@@ -3,5 +3,5 @@ import 'package:get/get.dart';
 class ClothPageController extends GetxController {
   var pageIndex = 0.obs;
 
-  setPage(idx) => pageIndex = idx;
+  setPage(idx) => pageIndex = idx.obs;
 }
