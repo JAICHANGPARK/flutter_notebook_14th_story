d16ab8b578911f84dea311c6130e6f96b52419e9
Dreamwalker <aristojeff@gmail.com>
2021-07-15 21:35:02 +0900

    Update clothes_home_page.dart

--- lib/ep851_clothes_app/ui/page/clothes_home_page.dart
+++ lib/ep851_clothes_app/ui/page/clothes_home_page.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
 class ClothesHomePage extends StatelessWidget {
-  const ClothesHomePage({Key? key}) : super(key: key);
+  ValueNotifier<int> _pageIndex = ValueNotifier<int>(0);
+  ClothesHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 72,
+          child: Row(
+            children: [
+
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
