82f82ef24a42712c5b52f0d6659d9f2768d9ec13
Dreamwalker <aristojeff@gmail.com>
2021-07-15 21:33:56 +0900

    Update clothes_app.dart

--- lib/ep851_clothes_app/clothes_app.dart
+++ lib/ep851_clothes_app/clothes_app.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+
+import 'ui/page/clothes_home_page.dart';
 class ClothesApp extends StatelessWidget {
   const ClothesApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ,
+      home: ClothesHomePage(),
     );
   }
 }
