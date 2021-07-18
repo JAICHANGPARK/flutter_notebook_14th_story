16f7a25aa966788f5558b4cc6c269e6d95d73d16
Dreamwalker <aristojeff@gmail.com>
2021-07-18 19:58:47 +0900

    Update clothes_detail_page.dart

---
+++ lib/ep851_clothes_app/ui/page/clothes_detail_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ClothesDetailPage extends StatefulWidget {
+  const ClothesDetailPage({Key? key}) : super(key: key);
+
+  @override
+  _ClothesDetailPageState createState() => _ClothesDetailPageState();
+}
+
+class _ClothesDetailPageState extends State<ClothesDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
