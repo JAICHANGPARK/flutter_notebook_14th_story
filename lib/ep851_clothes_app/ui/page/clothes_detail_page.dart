8b9473a36090c469b747213a056ba718994ec1a2
Dreamwalker <aristojeff@gmail.com>
2021-07-18 20:11:29 +0900

    Update clothes_detail_page.dart

--- lib/ep851_clothes_app/ui/page/clothes_detail_page.dart
+++ lib/ep851_clothes_app/ui/page/clothes_detail_page.dart
@@ -4,10 +4,8 @@ class ClothesDetailPage extends StatefulWidget {
   final String? img;
   final String? title;
   final double? price;
-   ClothesDetailPage({Key? key,
-   this.img,
-   this.title,
-   this.price}) : super(key: key);
+
+  ClothesDetailPage({Key? key, this.img, this.title, this.price}) : super(key: key);
 
   @override
   _ClothesDetailPageState createState() => _ClothesDetailPageState();
@@ -17,53 +15,51 @@ class _ClothesDetailPageState extends State<ClothesDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(child: Placeholder(), flex: 10,),
-            Expanded(child: Placeholder(), flex: 5,),
-            Expanded(child: Padding(
+        body: SafeArea(
+      child: Column(
+        children: [
+          Expanded(
+            child: Placeholder(),
+            flex: 10,
+          ),
+          Expanded(
+            child: Placeholder(),
+            flex: 5,
+          ),
+          Expanded(
+            child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Row(
                 children: [
                   Column(
-                    children: [
-                      Text("Price"),
-                      Text("\$79.99")
-                    ],
+                    children: [Text("Price"), Text("\$79.99")],
                   ),
-                  SizedBox(width: 24,),
-                  Expanded(child:Container(
-                    decoration: BoxDecoration(
-                      color: Colors.orangeAccent,
-                      borderRadius: BorderRadius.circular(16)
-                    ),
+                  SizedBox(
+                    width: 24,
+                  ),
+                  Expanded(
+                      child: Container(
+                    decoration: BoxDecoration(color: Colors.orangeAccent, borderRadius: BorderRadius.circular(16)),
                     child: Row(
                       children: [
-
+                        Text(
+                          "Add to Cart",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Icon(
+                          Icons.shopping_cart_outlined,
+                          color: Colors.white,
+                        )
                       ],
                     ),
                   )),
                 ],
               ),
-            ), flex: 2,),
-          ],
-        ),
-      )
-    );
+            ),
+            flex: 2,
+          ),
+        ],
+      ),
+    ));
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
