a7fe3214fda820bcf521476e07e8966f7fa08c82
Dreamwalker <aristojeff@gmail.com>
2021-07-20 20:30:33 +0900

    Update hiking_app.dart

--- lib/ep856_hiking_app/hiking_app.dart
+++ lib/ep856_hiking_app/hiking_app.dart
@@ -5,6 +5,8 @@ class HikingApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: HikingHomeApp(),
+    );
   }
 }
