b1e30c166751bbf2c997c4841597ee408b250e23
Dreamwalker <aristojeff@gmail.com>
2021-07-20 20:30:55 +0900

    Create hiking_home_app.dart

---
+++ lib/ep856_hiking_app/ui/hiking_home_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HikingHomeApp extends StatefulWidget {
+  HikingHomeApp({Key? key}) : super(key: key);
+
+  @override
+  _HikingHomeAppState createState() => _HikingHomeAppState();
+}
+
+class _HikingHomeAppState extends State<HikingHomeApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
