db0c12c8c38614b57bf6bcd2e7ba8750db7c0b57
Dreamwalker <aristojeff@gmail.com>
2021-07-20 20:30:17 +0900

    Update hiking_app.dart

--- lib/ep856_hiking_app/hiking_app.dart
+++ lib/ep856_hiking_app/hiking_app.dart
@@ -1,2 +1,10 @@
 import 'package:flutter/material.dart';
 
+class HikingApp extends StatelessWidget {
+  const HikingApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
