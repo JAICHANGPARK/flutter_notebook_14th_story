4adfb8564c94d7f534983393263e4b1d8a1a99f7
Dreamwalker <aristojeff@gmail.com>
2021-07-23 20:23:59 +0900

    Update hiking_detail_page.dart

--- lib/ep856_hiking_app/ui/hiking_detail_page.dart
+++ lib/ep856_hiking_app/ui/hiking_detail_page.dart
@@ -8,18 +8,20 @@ class HikingDetailPage extends StatelessWidget {
     return Scaffold(
       body: NestedScrollView(
         body: Column(
-          children: [
-
-          ],
+          children: [],
         ),
         headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
           return [
-            const SliverAppBar(
+            SliverAppBar(
+              expandedHeight: MediaQuery.of(context).size.height / 2.5,
+              flexibleSpace: Stack(
+                children: [
 
-            )
+                ],
+              ),
+            ),
           ];
         },
-
       ),
     );
   }
