39750e4352d597a2f5f99e5b0125b99efe931492
Dreamwalker <aristojeff@gmail.com>
2021-07-25 19:24:20 +0900

    Update medic_care_app.dart

--- lib/ep861_medic_care_app/medic_care_app.dart
+++ lib/ep861_medic_care_app/medic_care_app.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'ui/medic_care_home_page.dart';
+
 class MedicCareApp extends StatelessWidget {
-  const MedicCareApp({Key? key}) : super(key: key);
+  MedicCareApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
