b426626d94d6f80c5b7c51ab0291fcafc039d881
Dreamwalker <aristojeff@gmail.com>
2021-07-25 19:24:02 +0900

    Update medic_care_home_page.dart

---
+++ lib/ep861_medic_care_app/ui/medic_care_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MedicCareHomePage extends StatefulWidget {
+  const MedicCareHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MedicCareHomePageState createState() => _MedicCareHomePageState();
+}
+
+class _MedicCareHomePageState extends State<MedicCareHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
