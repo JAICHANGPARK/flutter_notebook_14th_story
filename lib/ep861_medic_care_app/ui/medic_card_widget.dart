cbd61ffc1e7a714fe2535091823729e97cb8f0ae
Dreamwalker <aristojeff@gmail.com>
2021-07-27 19:37:27 +0900

    Update medic_card_widget.dart

--- lib/ep861_medic_care_app/ui/medic_card_widget.dart
+++ lib/ep861_medic_care_app/ui/medic_card_widget.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+
+class MedicCardWidget extends StatelessWidget {
+  const MedicCardWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Theme.of(context).cardColor,
+      child: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
