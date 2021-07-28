d6c1fc87e3e99287a6801cd72fefcfe3ba413d36
Dreamwalker <aristojeff@gmail.com>
2021-07-28 19:57:14 +0900

    Update medical_home_page.dart

--- lib/ep864_medical_app/ui/medical_home_page.dart
+++ lib/ep864_medical_app/ui/medical_home_page.dart
@@ -11,6 +11,7 @@ class _MedicalHomePageState extends State<MedicalHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff27272c),
         body: SafeArea(
       child: Stack(
         children: [
