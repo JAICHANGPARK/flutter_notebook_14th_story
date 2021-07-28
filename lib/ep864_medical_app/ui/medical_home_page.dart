e15713d33f6b4a4a39d9a31339793a97ebe691d9
Dreamwalker <aristojeff@gmail.com>
2021-07-28 19:52:34 +0900

    Update medical_home_page.dart

---
+++ lib/ep864_medical_app/ui/medical_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MedicalHomePage extends StatefulWidget {
+  const MedicalHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MedicalHomePageState createState() => _MedicalHomePageState();
+}
+
+class _MedicalHomePageState extends State<MedicalHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
