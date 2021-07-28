ccb403c36d3e22ba7225bb71c681fa8b604c548f
Dreamwalker <aristojeff@gmail.com>
2021-07-28 19:52:00 +0900

    Update medical_app.dart

--- lib/ep864_medical_app/medical_app.dart
+++ lib/ep864_medical_app/medical_app.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+
+class MedicalApp extends StatelessWidget {
+  const MedicalApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ,
+    );
+  }
+}
