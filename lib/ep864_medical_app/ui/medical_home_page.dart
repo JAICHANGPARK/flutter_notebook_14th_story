3a7678337df8b9191986065b5c3a3afb62ab96a6
Dreamwalker <aristojeff@gmail.com>
2021-07-29 20:12:02 +0900

    Update medical_home_page.dart

--- lib/ep864_medical_app/ui/medical_home_page.dart
+++ lib/ep864_medical_app/ui/medical_home_page.dart
@@ -24,42 +24,55 @@ class _MedicalHomePageState extends State<MedicalHomePage> {
                 child: Column(
                   children: [
                     Expanded(
-                        flex: 5,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Expanded(
-                              child: Row(
-                                children: [
-                                  Expanded(child: Placeholder(),),
-                                  Expanded(child: Placeholder(),),
-                                  Expanded(child: Placeholder(),),
-                                ],
-                              ),
+                      flex: 5,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  child: Placeholder(),
+                                ),
+                                Expanded(
+                                  child: Placeholder(),
+                                ),
+                                Expanded(
+                                  child: Placeholder(),
+                                ),
+                              ],
                             ),
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: const [
-                                  Text("Good morning. Dr. Dream",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold
-                                  ),),
-                                  Text("You have 15 patients today"),
-                                ],
-                              ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: const [
+                                Text(
+                                  "Good morning. Dr. Dream",
+                                  style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(
+                                  height: 4,
+                                ),
+                                Text("You have 15 patients today",
+                                    style: TextStyle(color: Colors.grey, fontSize: 20, fontWeight: FontWeight.normal)),
+                              ],
                             ),
-                          ],
-                        ),),
+                          ),
+                        ],
+                      ),
+                    ),
                     Expanded(
                         flex: 4,
-                        child: Placeholder(color: Colors.purple,)),
+                        child: Placeholder(
+                          color: Colors.purple,
+                        )),
                     Expanded(
                         flex: 10,
-                        child: Placeholder(color: Colors.purple,))
+                        child: Placeholder(
+                          color: Colors.purple,
+                        ))
                   ],
                 ),
                 left: 0,
@@ -77,17 +90,36 @@ class _MedicalHomePageState extends State<MedicalHomePage> {
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        IconButton(onPressed: (){}, icon: Icon(Icons.home), color: Colors.white,),
-                        IconButton(onPressed: (){}, icon: Icon(Icons.card_membership), color: Colors.white,),
-                        SizedBox(width: 64,),
-                        IconButton(onPressed: (){}, icon: Icon(Icons.person), color: Colors.white,),
-                        IconButton(onPressed: (){}, icon: Icon(Icons.settings), color: Colors.white,),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.home),
+                          color: Colors.white,
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.card_membership),
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 64,
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.person),
+                          color: Colors.white,
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.settings),
+                          color: Colors.white,
+                        ),
                       ],
                     ),
                   )),
               Positioned(
                 child: Center(
-                  child: FloatingActionButton(onPressed: () {  },
+                  child: FloatingActionButton(
+                    onPressed: () {},
                     backgroundColor: deepPurpleColor,
                     child: Icon(Icons.add),
                     foregroundColor: Colors.white,
