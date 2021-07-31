92bbac65758ab8d1075d2c8f97631532a524f54d
Dreamwalker <aristojeff@gmail.com>
2021-07-31 20:20:37 +0900

    Create schedule.dart

---
+++ lib/ep864_medical_app/model/schedule.dart
@@ -0,0 +1,10 @@
+class DoctorSchedule {
+  String? avatarImg;
+  String? title;
+  String? subtitle;
+  String? timeIndex;
+  String? time;
+  bool? isDone;
+
+  DoctorSchedule(this.avatarImg, this.title, this.subtitle, this.timeIndex, this.time, this.isDone);
+}
