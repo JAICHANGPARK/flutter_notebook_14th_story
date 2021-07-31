66bc16c9f230869804be7c635b1d9189faebae78
Dreamwalker <aristojeff@gmail.com>
2021-07-31 20:28:13 +0900

    Create mock_schedule.dart

---
+++ lib/ep864_medical_app/mock/mock_schedule.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_notebook_14th_story/ep864_medical_app/model/schedule.dart';
+
+List<DoctorSchedule> doctorScheduleItems = [
+  DoctorSchedule(
+      avatarImg: "https://cdn.pixabay.com/photo/2018/08/23/22/29/girl-3626901__340.jpg",
+      isDone: true,
+      time: "08:15 - 08:40",
+      title: "Sarah Hosten",
+      subtitle: "27 yo Allergy",
+      timeIndex: "08:00"),
+  DoctorSchedule(
+    avatarImg: "https://cdn.pixabay.com/photo/2017/04/06/19/34/girl-2209147_960_720.jpg",
+      isDone: true,
+      time: "08:15 - 08:40",
+      title: "Sarah Hosten",
+      subtitle: "27 yo Allergy",
+      timeIndex: "09:00"
+
+  ),
+  DoctorSchedule(
+    avatarImg: "https://cdn.pixabay.com/photo/2017/05/12/11/29/girl-2306829_960_720.jpg",
+      isDone: true,
+      time: "08:15 - 08:40",
+      title: "Sarah Hosten",
+      subtitle: "27 yo Allergy",
+      timeIndex: "10:00"
+  ),
+];
