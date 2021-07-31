e9cad2ee19323664ccdbffd10edaee418bf1c028
Dreamwalker <aristojeff@gmail.com>
2021-07-31 20:37:29 +0900

    Update mock_schedule.dart

--- lib/ep864_medical_app/mock/mock_schedule.dart
+++ lib/ep864_medical_app/mock/mock_schedule.dart
@@ -9,20 +9,17 @@ List<DoctorSchedule> doctorScheduleItems = [
       subtitle: "27 yo Allergy",
       timeIndex: "08:00"),
   DoctorSchedule(
-    avatarImg: "https://cdn.pixabay.com/photo/2017/04/06/19/34/girl-2209147_960_720.jpg",
+      avatarImg: "https://cdn.pixabay.com/photo/2017/04/06/19/34/girl-2209147_960_720.jpg",
       isDone: true,
       time: "08:15 - 08:40",
       title: "Sarah Hosten",
       subtitle: "27 yo Allergy",
-      timeIndex: "09:00"
-
-  ),
+      timeIndex: "09:00"),
   DoctorSchedule(
-    avatarImg: "https://cdn.pixabay.com/photo/2017/05/12/11/29/girl-2306829_960_720.jpg",
+      avatarImg: "https://cdn.pixabay.com/photo/2017/05/12/11/29/girl-2306829_960_720.jpg",
       isDone: true,
       time: "08:15 - 08:40",
       title: "Sarah Hosten",
       subtitle: "27 yo Allergy",
-      timeIndex: "10:00"
-  ),
+      timeIndex: "10:00"),
 ];
