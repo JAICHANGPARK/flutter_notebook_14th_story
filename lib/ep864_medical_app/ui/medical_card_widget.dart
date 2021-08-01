cc45c7d04c00458931a84d597553eda79006ca75
Dreamwalker <aristojeff@gmail.com>
2021-08-01 18:46:00 +0900

    Update medical_card_widget.dart

--- lib/ep864_medical_app/ui/medical_card_widget.dart
+++ lib/ep864_medical_app/ui/medical_card_widget.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_14th_story/ep864_medical_app/model/schedule.dart';
 
 class MedicalCardWidget extends StatelessWidget {
+  DoctorSchedule  _doctorSchedule;
   const MedicalCardWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SizedBox(
+      height: 140,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+              flex: 3,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: Text(
+                    doctorScheduleItems[index].timeIndex ?? "-",
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+              )),
+          Expanded(
+              flex: 10,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.08),
+                    borderRadius: BorderRadius.circular(24)),
+              )),
+        ],
+      ),
+    );
   }
 }
