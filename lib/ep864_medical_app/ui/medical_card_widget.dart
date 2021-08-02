bc859b9c7a75fe9f229e2bd41205d66ff252db18
Dreamwalker <aristojeff@gmail.com>
2021-08-02 19:38:44 +0900

    Update medical_card_widget.dart

--- lib/ep864_medical_app/ui/medical_card_widget.dart
+++ lib/ep864_medical_app/ui/medical_card_widget.dart
@@ -8,91 +8,96 @@ class MedicalCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 140,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-              flex: 3,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Align(
-                  alignment: Alignment.topCenter,
-                  child: Text(
-                    doctorSchedule.timeIndex ?? "-",
-                    style: const TextStyle(color: Colors.white),
-                  ),
-                ),
-              )),
-          Expanded(
-              flex: 10,
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    CircleAvatar(
-                      backgroundImage: NetworkImage(doctorSchedule.avatarImg!),
+    return InkWell(
+      onTap: (){
+        
+      },
+      child: SizedBox(
+        height: 140,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+                flex: 3,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Text(
+                      doctorSchedule.timeIndex ?? "-",
+                      style: const TextStyle(color: Colors.white),
                     ),
-                    Expanded(
-                        child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 12),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    "${doctorSchedule.title}",
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                                  SizedBox(height: 4),
-                                  Text(
-                                    "${doctorSchedule.subtitle}",
-                                    style: TextStyle(color: Colors.white.withOpacity(0.2), fontSize: 12),
-                                  ),
-                                ],
-                              ),
-                              doctorSchedule.isDone!
-                                  ? Container(
-                                      height: 32,
-                                      width: 32,
-                                      decoration: BoxDecoration(
-                                          color: Colors.greenAccent, borderRadius: BorderRadius.circular(6)),
-                                      child: const Center(
-                                        child: Icon(
-                                          Icons.done,
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                    )
-                                  : Container(
-                                      height: 32,
-                                      width: 32,
-                                      decoration: BoxDecoration(
-                                          color: Colors.white.withOpacity(0.2), borderRadius: BorderRadius.circular(6)),
-                                    )
-                            ],
-                          ),
-                          Text(
-                            "${doctorSchedule.time}",
-                            style: TextStyle(color: Colors.white.withOpacity(0.2), fontSize: 12),
-                          )
-                        ],
+                  ),
+                )),
+            Expanded(
+                flex: 10,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CircleAvatar(
+                        backgroundImage: NetworkImage(doctorSchedule.avatarImg!),
                       ),
-                    ))
-                  ],
-                ),
-                decoration:
-                    BoxDecoration(color: Colors.white.withOpacity(0.08), borderRadius: BorderRadius.circular(24)),
-              )),
-        ],
+                      Expanded(
+                          child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 12),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "${doctorSchedule.title}",
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                    SizedBox(height: 4),
+                                    Text(
+                                      "${doctorSchedule.subtitle}",
+                                      style: TextStyle(color: Colors.white.withOpacity(0.2), fontSize: 12),
+                                    ),
+                                  ],
+                                ),
+                                doctorSchedule.isDone!
+                                    ? Container(
+                                        height: 32,
+                                        width: 32,
+                                        decoration: BoxDecoration(
+                                            color: Colors.greenAccent, borderRadius: BorderRadius.circular(6)),
+                                        child: const Center(
+                                          child: Icon(
+                                            Icons.done,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      )
+                                    : Container(
+                                        height: 32,
+                                        width: 32,
+                                        decoration: BoxDecoration(
+                                            color: Colors.white.withOpacity(0.2), borderRadius: BorderRadius.circular(6)),
+                                      )
+                              ],
+                            ),
+                            Text(
+                              "${doctorSchedule.time}",
+                              style: TextStyle(color: Colors.white.withOpacity(0.2), fontSize: 12),
+                            )
+                          ],
+                        ),
+                      ))
+                    ],
+                  ),
+                  decoration:
+                      BoxDecoration(color: Colors.white.withOpacity(0.08), borderRadius: BorderRadius.circular(24)),
+                )),
+          ],
+        ),
       ),
     );
   }
