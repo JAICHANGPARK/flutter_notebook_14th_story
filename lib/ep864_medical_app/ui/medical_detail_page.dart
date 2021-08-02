ecff0cf76bfc820b020a40a028677bf80699b315
Dreamwalker <aristojeff@gmail.com>
2021-08-02 19:37:23 +0900

    Update medical_detail_page.dart

---
+++ lib/ep864_medical_app/ui/medical_detail_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MedicalDetailPage extends StatelessWidget {
+  const MedicalDetailPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
