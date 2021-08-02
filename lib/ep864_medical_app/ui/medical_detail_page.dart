30217b6b371f1dfeab1c111e0d17ba0d0a939445
Dreamwalker <aristojeff@gmail.com>
2021-08-02 19:43:09 +0900

    Update medical_detail_page.dart

--- lib/ep864_medical_app/ui/medical_detail_page.dart
+++ lib/ep864_medical_app/ui/medical_detail_page.dart
@@ -3,23 +3,36 @@ import 'package:flutter/material.dart';
 import 'medical_home_page.dart';
 
 class MedicalDetailPage extends StatelessWidget {
-
   const MedicalDetailPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
-      body: Column(
-        children: [
-          Row(
-            children: [
-              IconButton(onPressed: (){}, icon: Icon(Icons.arrow_back_ios_new_outlined)),
-              IconButton(onPressed: (){}, icon: Icon(Icons.edit), ),
-            ],
-          ),
-          Expanded(child: Placeholder()),
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.arrow_back_ios_new_outlined),
+                    color: Colors.white,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.edit),
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+            ),
+            Expanded(child: Placeholder()),
+          ],
+        ),
       ),
     );
   }
