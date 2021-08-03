eeb2cd44a82a0e0cdb9a464ad0c3b9ccf4539285
Dreamwalker <aristojeff@gmail.com>
2021-08-03 20:12:13 +0900

    Update medical_app.dart

--- lib/ep864_medical_app/medical_app.dart
+++ lib/ep864_medical_app/medical_app.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 
 import 'ui/m_detail_page.dart';
 
+Color backgroundColor = Color(0xff27272c);
+Color deepDarkColor = Color(0xff17171b);
+Color deepPurpleColor = Color(0xff9292e4);
+Color lightPurpleColor = Color(0xffb9b9e3);
+
 
 class MedicalApp extends StatelessWidget {
   MedicalApp({Key? key}) : super(key: key);
