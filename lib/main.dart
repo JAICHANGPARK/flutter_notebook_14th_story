53b8f44427688492a0bc940c3f009349f5e559a3
Dreamwalker <aristojeff@gmail.com>
2021-08-03 19:57:59 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
 import 'ep864_medical_app/medical_app.dart';
 
-void main() => runApp(MedicalApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MedicalApp());
+}
