b9d8c8c4cff07a6126154f3566d43b7d90f81ed3
Dreamwalker <aristojeff@gmail.com>
2021-08-03 20:11:37 +0900

    Update m_detail_page.dart

--- lib/ep864_medical_app/ui/m_detail_page.dart
+++ lib/ep864_medical_app/ui/m_detail_page.dart
@@ -6,11 +6,70 @@ class MDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [
-
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.arrow_back_ios_new_outlined),
+                    color: Colors.white,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.edit),
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 32,
+                  ),
+                  Column(
+                    children: [
+                      Text("Dreamwalker"),
+                      Text("27 yo Flutter Lover")
+                    ],
+                  )
+                ],
+              ),
+              flex: 3,
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(child: Placeholder()),
+                  Expanded(child: Placeholder()),
+                ],
+              ),
+              flex: 6,
+            ),
+            Expanded(
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 5,
+                  itemBuilder: (context, index) {
+                    return Container();
+                  }),
+              flex: 2,
+            ),
+            Expanded(
+              child: Container(
+                decoration:
+                BoxDecoration(borderRadius: BorderRadius.circular(36), color: Colors.white.withOpacity(0.1)),
+              ),
+              flex: 8,
+            ),
+          ],
+        ),
       ),
     );
   }
