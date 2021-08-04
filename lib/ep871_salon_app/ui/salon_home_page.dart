3c1275af803f2bbbd7afccc379c70293d85718d3
Dreamwalker <aristojeff@gmail.com>
2021-08-04 19:18:26 +0900

    Create salon_home_page.dart

---
+++ lib/ep871_salon_app/ui/salon_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SalonHomePage extends StatefulWidget {
+  const SalonHomePage({Key? key}) : super(key: key);
+
+  @override
+  _SalonHomePageState createState() => _SalonHomePageState();
+}
+
+class _SalonHomePageState extends State<SalonHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
