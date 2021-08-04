83f59478faefe6a64e1d6ffa857a150ba8df4bad
Dreamwalker <aristojeff@gmail.com>
2021-08-04 19:16:51 +0900

    Create salon_management_app.dart

---
+++ lib/ep871_salon_app/salon_management_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class SalonManagementApp extends StatelessWidget {
+  const SalonManagementApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MateiralApp();
+  }
+}
