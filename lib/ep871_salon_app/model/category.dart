8211108230505cfaef810c022379fe0c5c11d39a
Dreamwalker <aristojeff@gmail.com>
2021-08-05 19:26:37 +0900

    Update category.dart

--- lib/ep871_salon_app/model/category.dart
+++ lib/ep871_salon_app/model/category.dart
@@ -2,3 +2,11 @@ class BookingCategory{
   String? title;
   BookingCategory(this.title);
 }
+
+List<BookingCategory> categoryItems = [
+  BookingCategory("Pending"),
+  BookingCategory("Accepted"),
+  BookingCategory("Completed"),
+  BookingCategory("Decline"),
+  BookingCategory("Other"),
+];
