957657091c573ee535c0c54946d8a0d2e0f37b8e
Dreamwalker <aristojeff@gmail.com>
2021-08-06 19:13:37 +0900

    Update booking.dart

--- lib/ep871_salon_app/model/booking.dart
+++ lib/ep871_salon_app/model/booking.dart
@@ -1,4 +1,4 @@
-class Booking{
+class Booking {
   String? code;
   String? title;
   String? moreTitle;
@@ -6,6 +6,13 @@ class Booking{
   int? price;
   bool? isPaid;
 
-  Booking({this.code, this.title, this.moreTitle, this.subTitle, this.price});
+  Booking({this.code, this.title, this.moreTitle, this.subTitle, this.price, this.isPaid});
 }
 
+List<Booking> bookingItems =[
+  Booking(code: "53623722", title: "Hair sps, Haircut(Women)", isPaid: false,
+  moreTitle: "+2 more",
+    price: 1000,
+    subTitle:"Arriving 7 Jun 2021, 08:11 PM"
+  ),
+];
