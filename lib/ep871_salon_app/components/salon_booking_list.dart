e6cb7d4fd39f209a745397bf1e7fdf4bfa69a61e
Dreamwalker <aristojeff@gmail.com>
2021-08-06 19:17:18 +0900

    Update salon_booking_list.dart

--- lib/ep871_salon_app/components/salon_booking_list.dart
+++ lib/ep871_salon_app/components/salon_booking_list.dart
@@ -1,26 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_14th_story/ep871_salon_app/model/booking.dart';
 
-
-
 class SalonBookingList extends StatelessWidget {
   const SalonBookingList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: (context, index){
-
-      return Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-                Text("BOOKING #${bookingItems[index].code}"),
-
-          ],
-        ),
-      );
-
-    },
-    itemCount: bookingItems.length,);
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("BOOKING #${bookingItems[index].code}"),
+              Row(
+                children: [
+                  Column(
+                    children: [
+                      Text("${bookingItems[index].title}"),
+                      Text("${bookingItems[index].moreTitle}"),
+                      Text("${bookingItems[index].subTitle}"),
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
+        );
+      },
+      itemCount: bookingItems.length,
+    );
   }
 }
