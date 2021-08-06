b3a8fa623cfa2398aad5128a27bedf2ce4e44ec0
Dreamwalker <aristojeff@gmail.com>
2021-08-06 19:11:36 +0900

    Update booking.dart

--- lib/ep871_salon_app/model/booking.dart
+++ lib/ep871_salon_app/model/booking.dart
@@ -1,7 +1,9 @@
 class Booking{
-  String code;
-  String title;
-  String moreTitle;
-  String subTitle;
-  int price;
+  String? code;
+  String? title;
+  String? moreTitle;
+  String? subTitle;
+  int? price;
+
+  Booking({this.code, this.title, this.moreTitle, this.subTitle, this.price});
 }
