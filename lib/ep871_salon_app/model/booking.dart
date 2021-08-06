f12af16ce1e85ef3bdd0a739523892c7b2975aca
Dreamwalker <aristojeff@gmail.com>
2021-08-06 19:14:16 +0900

    Update booking.dart

--- lib/ep871_salon_app/model/booking.dart
+++ lib/ep871_salon_app/model/booking.dart
@@ -9,10 +9,33 @@ class Booking {
   Booking({this.code, this.title, this.moreTitle, this.subTitle, this.price, this.isPaid});
 }
 
-List<Booking> bookingItems =[
-  Booking(code: "53623722", title: "Hair sps, Haircut(Women)", isPaid: false,
-  moreTitle: "+2 more",
-    price: 1000,
-    subTitle:"Arriving 7 Jun 2021, 08:11 PM"
-  ),
+List<Booking> bookingItems = [
+  Booking(
+      code: "53623722",
+      title: "Hair sps, Haircut(Women)",
+      isPaid: false,
+      moreTitle: "+2 more",
+      price: 1000,
+      subTitle: "Arriving 7 Jun 2021, 08:11 PM"),
+  Booking(
+      code: "53623753",
+      title: "Keratin Treatment",
+      isPaid: true,
+      moreTitle: "+2 more",
+      price: 1000,
+      subTitle: "Arriving 7 Jun 2021, 08:11 PM"),
+  Booking(
+      code: "53623722",
+      title: "Hair sps, Haircut(Women)",
+      isPaid: false,
+      moreTitle: "+2 more",
+      price: 1000,
+      subTitle: "Arriving 7 Jun 2021, 08:11 PM"),
+  Booking(
+      code: "53623722",
+      title: "Hair sps, Haircut(Women)",
+      isPaid: false,
+      moreTitle: "+2 more",
+      price: 1000,
+      subTitle: "Arriving 7 Jun 2021, 08:11 PM"),
 ];
