74c0392f93237c2b25ddb1ec4f5e50a6ae7ed156
Dreamwalker <aristojeff@gmail.com>
2021-08-06 19:24:28 +0900

    Update salon_booking_list.dart

--- lib/ep871_salon_app/components/salon_booking_list.dart
+++ lib/ep871_salon_app/components/salon_booking_list.dart
@@ -16,56 +16,82 @@ class SalonBookingList extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text("BOOKING #${bookingItems[index].code}", style: TextStyle(fontSize: 12),),
-                SizedBox(height: 8,),
+                Text(
+                  "BOOKING #${bookingItems[index].code}",
+                  style: TextStyle(fontSize: 12),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text("${bookingItems[index].title}", style: TextStyle(fontSize: 12),),
-                        Text("${bookingItems[index].moreTitle}", style: TextStyle(fontSize: 10),),
-                        SizedBox(height: 8,),
-                        Text("${bookingItems[index].subTitle}", style: TextStyle(fontSize: 10,
-                        color: Colors.grey),),
+                        Text(
+                          "${bookingItems[index].title}",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                        Text(
+                          "${bookingItems[index].moreTitle}",
+                          style: TextStyle(fontSize: 10),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          "${bookingItems[index].subTitle}",
+                          style: TextStyle(fontSize: 10, color: Colors.grey),
+                        ),
                       ],
                     ),
                     Container(
-                      decoration: BoxDecoration(
-                        color: Colors.grey[200],
-                        borderRadius: BorderRadius.circular(8)
-                      ),
+                      decoration: BoxDecoration(color: Colors.grey[200], borderRadius: BorderRadius.circular(8)),
                       padding: EdgeInsets.all(8),
-                      child: Text("\$ ${bookingItems[index].price}",style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16
-                      ),),
+                      child: Text(
+                        "\$ ${bookingItems[index].price}",
+                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
                     )
                   ],
                 ),
-                SizedBox(height: 12,),
-                bookingItems[index].isPaid ?? false ? Container(
-                  decoration: BoxDecoration(
-                    color: Colors.green,
-                    borderRadius: BorderRadius.circular(4)
-                  ),
-                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                  child: Text("10% ADVANCE PAID", style: TextStyle(
-                    fontSize: 12,
-                    color: Colors.white
-                  ),),
-                ) : Container(),
-                SizedBox(height: 23,),
+                SizedBox(
+                  height: 12,
+                ),
+                bookingItems[index].isPaid ?? false
+                    ? Container(
+                        decoration: BoxDecoration(color: Colors.green, borderRadius: BorderRadius.circular(4)),
+                        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                        child: Text(
+                          "10% ADVANCE PAID",
+                          style: TextStyle(fontSize: 12, color: Colors.white),
+                        ),
+                      )
+                    : Container(),
+                SizedBox(
+                  height: 23,
+                ),
                 SizedBox(
                   height: 42,
                   child: Row(
                     children: [
-                      Expanded(child: Container(
-                        color: Colors.red[100],
-                      )),SizedBox(width: 8,),
-                      Expanded(child: Container(
-                        color: Colors.grey[200],
+                      Expanded(
+                          child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(4),
+                          color: Colors.red[100],
+                        ),
+                      )),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Expanded(
+                          child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(4),
+                          color: Colors.grey[200],
+                        ),
                       )),
                     ],
                   ),
