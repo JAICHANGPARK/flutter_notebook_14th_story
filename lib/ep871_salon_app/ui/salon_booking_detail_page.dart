9fc73284031974cf1f12ffb91e724450757934ca
Dreamwalker <aristojeff@gmail.com>
2021-08-07 21:22:35 +0900

    Update salon_booking_detail_page.dart

---
+++ lib/ep871_salon_app/ui/salon_booking_detail_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class SalonBookingDetailPage extends StatelessWidget {
+  const SalonBookingDetailPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
