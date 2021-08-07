68d5b965e39d24d075e74bcd68dd836442c8a309
Dreamwalker <aristojeff@gmail.com>
2021-08-07 21:37:04 +0900

    Update booking_staff_widget.dart

--- lib/ep871_salon_app/components/booking_staff_widget.dart
+++ lib/ep871_salon_app/components/booking_staff_widget.dart
@@ -1,18 +1,52 @@
 import 'package:flutter/material.dart';
 
-
 class BookingStaffWidget extends StatelessWidget {
   const BookingStaffWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        color: Colors.white
-      ),
+      decoration: const BoxDecoration(color: Colors.white),
+      padding: const EdgeInsets.all(16),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          Text(
+            "Staff".toUpperCase(),
+            style: TextStyle(fontSize: 12, color: Colors.grey),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 12),
+            child: Row(
+              children: [
+                Container(
+                  height: 48,
+                  width: 48,
+                  decoration: BoxDecoration(color: Colors.blue, borderRadius: BorderRadius.circular(16)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Haircut(Women)",
+                        style: TextStyle(fontSize: 14),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        "Duration 30 Mins",
+                        style: TextStyle(fontSize: 12, color: Colors.grey),
+                      )
+                    ],
+                  ),
+                ),
 
+              ],
+            ),
+          )
         ],
       ),
     );
