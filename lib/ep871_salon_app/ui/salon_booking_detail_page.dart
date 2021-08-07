d02d19c6bc8a74343beb7ba44dc9357101e8ae0f
Dreamwalker <aristojeff@gmail.com>
2021-08-07 21:35:58 +0900

    Update salon_booking_detail_page.dart

--- lib/ep871_salon_app/ui/salon_booking_detail_page.dart
+++ lib/ep871_salon_app/ui/salon_booking_detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_14th_story/ep871_salon_app/components/booking_staff_widget.dart';
 import 'package:flutter_notebook_14th_story/ep871_salon_app/components/salon_booking_detail_service_widget.dart';
 
 class SalonBookingDetailPage extends StatelessWidget {
@@ -24,7 +25,10 @@ class SalonBookingDetailPage extends StatelessWidget {
         child: Column(
           children: [
             SalonBookingDetailServiceWidget(),
-
+            SizedBox(
+              height: 8,
+            ),
+            BookingStaffWidget(),
           ],
         ),
       ),
