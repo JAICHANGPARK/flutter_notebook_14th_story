e63493d22d22764f78e727438f43838cebb22157
Dreamwalker <aristojeff@gmail.com>
2021-08-07 21:28:04 +0900

    Update salon_booking_detail_page.dart

--- lib/ep871_salon_app/ui/salon_booking_detail_page.dart
+++ lib/ep871_salon_app/ui/salon_booking_detail_page.dart
@@ -1,27 +1,26 @@
 import 'package:flutter/material.dart';
 
-
 class SalonBookingDetailPage extends StatelessWidget {
   String title;
+
   SalonBookingDetailPage({Key? key, required this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        
+        elevation: 1,
+        iconTheme: IconTheme.of(context).copyWith(color: Colors.black),
         backgroundColor: Colors.white,
         centerTitle: true,
-        title: Text("Booking $title",style: TextStyle(
-          color: Colors.black,
-          fontSize: 14
-        ),),
+        title: Text(
+          "Booking $title",
+          style: TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold),
+        ),
       ),
       body: SingleChildScrollView(
         child: Column(
-          children: [
-
-          ],
+          children: [],
         ),
       ),
     );
