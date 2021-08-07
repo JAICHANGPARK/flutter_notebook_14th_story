d92eb4a5f57abe6db2267c69cf4f082bac1ccb67
Dreamwalker <aristojeff@gmail.com>
2021-08-07 21:24:14 +0900

    Update salon_booking_detail_page.dart

--- lib/ep871_salon_app/ui/salon_booking_detail_page.dart
+++ lib/ep871_salon_app/ui/salon_booking_detail_page.dart
@@ -10,7 +10,7 @@ class SalonBookingDetailPage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            
+
           ],
         ),
       ),
