e35729fda77cb3714212139602a22c279ffc4a29
Dreamwalker <aristojeff@gmail.com>
2021-08-07 21:35:56 +0900

    Create booking_staff_widget.dart

---
+++ lib/ep871_salon_app/components/booking_staff_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+class BookingStaffWidget extends StatelessWidget {
+  const BookingStaffWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white
+      ),
+      child: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
