5a576a03d4bdc93d53eb46472bdf71276c13dc8a
Dreamwalker <aristojeff@gmail.com>
2021-08-07 21:30:53 +0900

    Update salon_booking_detail_service_widget.dart

--- lib/ep871_salon_app/components/salon_booking_detail_service_widget.dart
+++ lib/ep871_salon_app/components/salon_booking_detail_service_widget.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-
 class SalonBookingDetailServiceWidget extends StatelessWidget {
   const SalonBookingDetailServiceWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: Colors.white,
       child: Column(
         children: [
           Text("Services".toUpperCase()),
@@ -19,8 +19,17 @@ class SalonBookingDetailServiceWidget extends StatelessWidget {
               ),
               Column(
                 children: [
-
+                  Text("Haircut(Women)"),
+                  Text("Duration 30 Mins")
                 ],
+              ),
+              Container(
+                decoration: BoxDecoration(color: Colors.grey[200], borderRadius: BorderRadius.circular(8)),
+                padding: EdgeInsets.all(8),
+                child: Text(
+                  "\$ ${19.99}",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
               )
             ],
           )
