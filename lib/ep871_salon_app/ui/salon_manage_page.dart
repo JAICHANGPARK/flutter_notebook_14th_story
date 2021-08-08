eea4228ae8c66c32053051d73ef60b4ad1711996
Dreamwalker <aristojeff@gmail.com>
2021-08-08 17:34:24 +0900

    Update salon_manage_page.dart

--- lib/ep871_salon_app/ui/salon_manage_page.dart
+++ lib/ep871_salon_app/ui/salon_manage_page.dart
@@ -6,27 +6,32 @@ class SalonManagePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Text("Manage Staff"),
         backgroundColor: Colors.white,
         foregroundColor: Colors.black,
         elevation: 1,
         centerTitle: true,
-        actions: [
-          IconButton(onPressed: (){}, icon:  Icon(Icons.add_box))
-
-        ],
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add_box))],
       ),
       body: Column(
         children: [
-          Expanded(
+          Container(
+            height: 64,
+            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            color: Colors.white,
             child: Container(
-              color: Colors.white,
-              child: Column(
-                children: [],
+              decoration: BoxDecoration(color: Colors.grey[100], borderRadius: BorderRadius.circular(16)),
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: TextField(
+                decoration:
+                    InputDecoration(border: InputBorder.none, hintText: "Search Staff", suffixIcon: Icon(Icons.search)),
               ),
             ),
-            flex: 2,
+          ),
+          SizedBox(
+            height: 8,
           ),
           Expanded(
             child: Placeholder(),
