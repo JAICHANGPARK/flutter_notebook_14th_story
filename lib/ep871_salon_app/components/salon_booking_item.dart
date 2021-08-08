7e077ba886899c438d457b67fcae655fa73a5483
Dreamwalker <aristojeff@gmail.com>
2021-08-08 17:21:42 +0900

    Update salon_booking_item.dart

--- lib/ep871_salon_app/components/salon_booking_item.dart
+++ lib/ep871_salon_app/components/salon_booking_item.dart
@@ -1,9 +1,10 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_14th_story/ep871_salon_app/model/booking.dart';
 
 class SalonBookingItem extends StatelessWidget {
-  
-  const SalonBookingItem({Key? key}) : super(key: key);
+  final Booking booking;
+
+  SalonBookingItem({Key? key, required this.booking}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
