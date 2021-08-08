aa7ee036703c232c7f57810503026eaa56d70de6
Dreamwalker <aristojeff@gmail.com>
2021-08-08 17:26:11 +0900

    Create salon_manage_page.dart

---
+++ lib/ep871_salon_app/ui/salon_manage_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SalonManagePage extends StatelessWidget {
+  const SalonManagePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+
+      ],
+    );
+  }
+}
