35e83756156111532176e1e3a35077304d936a1c
Dreamwalker <aristojeff@gmail.com>
2021-08-08 17:30:26 +0900

    Update salon_manage_page.dart

--- lib/ep871_salon_app/ui/salon_manage_page.dart
+++ lib/ep871_salon_app/ui/salon_manage_page.dart
@@ -1,28 +1,39 @@
 import 'package:flutter/material.dart';
 
-
 class SalonManagePage extends StatelessWidget {
   const SalonManagePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Manage Staff"),),
-      body: Column(
-        children: [
-          Expanded(child: Container(
-            color: Colors.white,
-            child: Column(
-              children: [
+      appBar: AppBar(
+        title: Text("Manage Staff"),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 1,
+        centerTitle: true,
+        actions: [
+          IconButton(onPressed: (){}, icon:  Icon(Icons.add_box))
 
-              ],
-            ),
-          ), flex: 2,),
-          Expanded(child: Placeholder(), flex: 10,),
         ],
       ),
-
-
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.white,
+              child: Column(
+                children: [],
+              ),
+            ),
+            flex: 2,
+          ),
+          Expanded(
+            child: Placeholder(),
+            flex: 10,
+          ),
+        ],
+      ),
     );
   }
 }
