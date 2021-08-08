350c7b8deeebefe07786f530475ee816d6674061
Dreamwalker <aristojeff@gmail.com>
2021-08-08 17:21:22 +0900

    Update salon_booking_item.dart

--- lib/ep871_salon_app/components/salon_booking_item.dart
+++ lib/ep871_salon_app/components/salon_booking_item.dart
@@ -1,1 +1,12 @@
 
+import 'package:flutter/material.dart';
+
+class SalonBookingItem extends StatelessWidget {
+  
+  const SalonBookingItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
