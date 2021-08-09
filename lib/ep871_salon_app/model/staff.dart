093b2f006a5d23a3cb22bcd46fe44e12d1086973
Dreamwalker <aristojeff@gmail.com>
2021-08-09 19:30:55 +0900

    Update staff.dart

--- lib/ep871_salon_app/model/staff.dart
+++ lib/ep871_salon_app/model/staff.dart
@@ -1,12 +1,20 @@
+import 'package:rpgnamegenerator/rpgnamegenerator.dart';
+
 class Staff {
   String? profileImg;
   String? name;
   String? subtitle;
   String? services;
-  Staff({this.profileImg, this.name, this.subtitle, this.services});
+  bool isOn;
+
+  Staff({this.profileImg, this.name, this.subtitle, this.services, this.isOn = true});
 }
 
 List<Staff> manageStaffItems = [
-  Staff(profileImg: "https://cdn.pixabay.com/photo/2017/12/01/08/02/paint-2990357__340.jpg" ,
-  name: ),
+  Staff(
+      profileImg: "https://cdn.pixabay.com/photo/2017/12/01/08/02/paint-2990357__340.jpg",
+      name: RpgNameGenerator.getName("human", 'male'),
+      subtitle: "stylist",
+      services: "Caters to all/23 Services",
+      isOn: true),
 ];
