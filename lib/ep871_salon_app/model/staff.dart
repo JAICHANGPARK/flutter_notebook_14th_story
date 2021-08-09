0edd1f306e9aaebb392fe456547c7b7bf13f7f41
Dreamwalker <aristojeff@gmail.com>
2021-08-09 19:29:02 +0900

    Update staff.dart

--- lib/ep871_salon_app/model/staff.dart
+++ lib/ep871_salon_app/model/staff.dart
@@ -1,6 +1,12 @@
 class Staff {
-  String profileImg;
-  String name;
-  String subtitle;
-  String services;
+  String? profileImg;
+  String? name;
+  String? subtitle;
+  String? services;
+  Staff({this.profileImg, this.name, this.subtitle, this.services});
 }
+
+List<Staff> manageStaffItems = [
+  Staff(profileImg: "https://cdn.pixabay.com/photo/2017/12/01/08/02/paint-2990357__340.jpg" ,
+  name: ),
+];
