dc29929bf4fd65c3b6bfcd4081743c63f9dd2dd2
Dreamwalker <aristojeff@gmail.com>
2021-08-10 19:09:33 +0900

    Create daa_home_page.dart

---
+++ lib/ep877_doctor_appointment_app/ui/daa_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class DAAHomePage extends StatefulWidget {
+  const DAAHomePage({Key? key}) : super(key: key);
+
+  @override
+  _DAAHomePageState createState() => _DAAHomePageState();
+}
+
+class _DAAHomePageState extends State<DAAHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
