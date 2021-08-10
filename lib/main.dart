3583191dce26a2a5c464902aa242b8ba6e19d411
Dreamwalker <aristojeff@gmail.com>
2021-08-10 19:10:23 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'ep871_salon_app/salon_management_app.dart';
+import 'ep877_doctor_appointment_app/dotor_appointment_app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(SalonManagementApp());
+  runApp(DoctorAppointmentApp());
 }
