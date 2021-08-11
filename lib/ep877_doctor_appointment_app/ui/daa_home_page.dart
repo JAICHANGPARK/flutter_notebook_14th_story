e4ff758eade3a43c698530f405bedb377386eb37
Dreamwalker <aristojeff@gmail.com>
2021-08-11 20:14:56 +0900

    Update daa_home_page.dart

--- lib/ep877_doctor_appointment_app/ui/daa_home_page.dart
+++ lib/ep877_doctor_appointment_app/ui/daa_home_page.dart
@@ -18,71 +18,95 @@ class _DAAHomePageState extends State<DAAHomePage> {
         children: [
           Expanded(
             flex: 20,
-            child: ValueListenableBuilder<int>(valueListenable: _bottomIndex, builder: (context, value, child){
-              if(value == 0) {
-                return Column(
-                  children: [
-                    Expanded(
-                      flex: 9,
-                      child: Container(
-                        color: Colors.teal[300],
-                        child: Column(
-                          children: [
-                            SizedBox(
-                              height: 64,
-                            ),
-                            Expanded(
-                                flex: 4,
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Column(
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        crossAxisAlignment: CrossAxisAlignment.start,
+            child: ValueListenableBuilder<int>(
+                valueListenable: _bottomIndex,
+                builder: (context, value, child) {
+                  if (value == 0) {
+                    return Column(
+                      children: [
+                        Expanded(
+                          flex: 9,
+                          child: Container(
+                            color: Colors.teal[300],
+                            child: Column(
+                              children: [
+                                SizedBox(
+                                  height: 64,
+                                ),
+                                Expanded(
+                                    flex: 4,
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                                      child: Row(
+                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                         children: [
-                                          Text("👋 Hello,", style: TextStyle(color: Colors.white)),
-                                          SizedBox(height: 12),
-                                          Text(
-                                            "Dreamwalker",
-                                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
-                                          )
+                                          Column(
+                                            mainAxisAlignment: MainAxisAlignment.center,
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              Text("👋 Hello,", style: TextStyle(color: Colors.white)),
+                                              SizedBox(height: 12),
+                                              Text(
+                                                "Dreamwalker",
+                                                style: TextStyle(
+                                                    fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
+                                              )
+                                            ],
+                                          ),
+                                          CircleAvatar()
                                         ],
                                       ),
-                                      CircleAvatar()
-                                    ],
-                                  ),
-                                )),
-                            Expanded(
-                                flex: 4,
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Container(
-                                    decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(32)),
-                                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                                    child: const Center(
-                                      child: const TextField(
-                                        decoration: InputDecoration(
-                                            border: InputBorder.none, hintText: "Search", icon: Icon(Icons.search)),
+                                    )),
+                                Expanded(
+                                    flex: 4,
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Container(
+                                        decoration:
+                                            BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(32)),
+                                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                                        child: const Center(
+                                          child: const TextField(
+                                            decoration: InputDecoration(
+                                                border: InputBorder.none, hintText: "Search", icon: Icon(Icons.search)),
+                                          ),
+                                        ),
                                       ),
-                                    ),
-                                  ),
-                                )),
-                            Expanded(flex: 6, child: Placeholder()),
-                          ],
+                                    )),
+                                Expanded(flex: 6, child: Placeholder()),
+                              ],
+                            ),
+                          ),
                         ),
+                        Expanded(flex: 12, child: Placeholder()),
+                      ],
+                    );
+                  } else if (value == 1)
+                    return Container(
+                      child: Center(
+                        child: Text("Page 1"),
                       ),
-                    ),
-                    Expanded(flex: 12, child: Placeholder()),
-                  ],
-                );
-              }else if(value == 1) return Container(child: Center(child: Text("Page2"),),);
-              else if(value == 2) return Container(child: Center(child: Text("Page3"),),);
-              else  return Container(child: Center(child: Text("else"),),);
-            }),
+                    );
+                  else if (value == 2)
+                    return Container(
+                      child: Center(
+                        child: Text("Page 2"),
+                      ),
+                    );
+                  else if (value == 3)
+                    return Container(
+                      child: Center(
+                        child: Text("Page 3"),
+                      ),
+                    );
+                  else
+                    return Container(
+                      child: Center(
+                        child: Text("else"),
+                      ),
+                    );
+                }),
           ),
-
           Expanded(
               flex: 2,
               child: ValueListenableBuilder<int>(
