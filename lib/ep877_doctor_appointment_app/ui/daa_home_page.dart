f3c7673dc279b0f1d06022a4a5ff8e29e3d9e004
Dreamwalker <aristojeff@gmail.com>
2021-08-11 20:12:35 +0900

    Update daa_home_page.dart

--- lib/ep877_doctor_appointment_app/ui/daa_home_page.dart
+++ lib/ep877_doctor_appointment_app/ui/daa_home_page.dart
@@ -16,59 +16,68 @@ class _DAAHomePageState extends State<DAAHomePage> {
       resizeToAvoidBottomInset: false,
       body: Column(
         children: [
-          Expanded(
-            flex: 9,
-            child: Container(
-              color: Colors.teal[300],
-              child: Column(
+          ValueListenableBuilder(valueListenable: _bottomIndex, builder: (context, value, child){
+            if(value == 0) {
+              return Column(
                 children: [
-                  SizedBox(
-                    height: 64,
-                  ),
                   Expanded(
-                      flex: 4,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text("👋 Hello,", style: TextStyle(color: Colors.white)),
-                                SizedBox(height: 12),
-                                Text(
-                                  "Dreamwalker",
-                                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
-                                )
-                              ],
-                            ),
-                            CircleAvatar()
-                          ],
-                        ),
-                      )),
-                  Expanded(
-                      flex: 4,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(32)),
-                          padding: const EdgeInsets.symmetric(horizontal: 16),
-                          child: const Center(
-                            child: const TextField(
-                              decoration: InputDecoration(
-                                  border: InputBorder.none, hintText: "Search", icon: Icon(Icons.search)),
-                            ),
+                    flex: 9,
+                    child: Container(
+                      color: Colors.teal[300],
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 64,
                           ),
-                        ),
-                      )),
-                  Expanded(flex: 6, child: Placeholder()),
+                          Expanded(
+                              flex: 4,
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text("👋 Hello,", style: TextStyle(color: Colors.white)),
+                                        SizedBox(height: 12),
+                                        Text(
+                                          "Dreamwalker",
+                                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
+                                        )
+                                      ],
+                                    ),
+                                    CircleAvatar()
+                                  ],
+                                ),
+                              )),
+                          Expanded(
+                              flex: 4,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(32)),
+                                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                                  child: const Center(
+                                    child: const TextField(
+                                      decoration: InputDecoration(
+                                          border: InputBorder.none, hintText: "Search", icon: Icon(Icons.search)),
+                                    ),
+                                  ),
+                                ),
+                              )),
+                          Expanded(flex: 6, child: Placeholder()),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(flex: 12, child: Placeholder()),
                 ],
-              ),
-            ),
-          ),
-          Expanded(flex: 12, child: Placeholder()),
+              );
+            }
+          }),
+
           Expanded(
               flex: 2,
               child: ValueListenableBuilder<int>(
