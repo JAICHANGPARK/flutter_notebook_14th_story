b8b9c571cabc37e923804f86e4a2e0d4203c6790
Dreamwalker <aristojeff@gmail.com>
2021-08-12 21:04:18 +0900

    Update diary.dart

--- lib/ep877_doctor_appointment_app/model/diary.dart
+++ lib/ep877_doctor_appointment_app/model/diary.dart
@@ -5,3 +5,19 @@ class Diary{
 
   Diary(this.weekend, this.day, this.isEvent);
 }
+
+
+
+List<Diary> calItems = List.generate(7, (index) {
+  DateTime dateTime = DateTime.now().subtract(Duration(days: 7-index),);
+ return Diary(weekToString(dateTime.weekday), dateTime.day.toString(), isEvent);
+});
+
+String weekToString(int week){
+  switch(week){
+    case 1:
+      DateTime.sunday
+      return ""
+  }
+
+}
