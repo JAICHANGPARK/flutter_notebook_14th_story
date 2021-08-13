ce040fe7679a07e6c9a2e597e96cb5894effec2a
Dreamwalker <aristojeff@gmail.com>
2021-08-13 22:41:51 +0900

    Create doctors_page.dart

---
+++ lib/ep877_doctor_appointment_app/ui/doctors_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class DoctorsPage extends StatefulWidget {
+  const DoctorsPage({Key? key}) : super(key: key);
+
+  @override
+  _DoctorsPageState createState() => _DoctorsPageState();
+}
+
+class _DoctorsPageState extends State<DoctorsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
