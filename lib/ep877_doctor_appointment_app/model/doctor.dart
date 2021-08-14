de7b2aed316189b39204a245ea697c4fba73774c
Dreamwalker <aristojeff@gmail.com>
2021-08-14 20:05:40 +0900

    Update doctor.dart

--- lib/ep877_doctor_appointment_app/model/doctor.dart
+++ lib/ep877_doctor_appointment_app/model/doctor.dart
@@ -1,11 +1,11 @@
 class Doctor {
-
   String? profileImg;
   String? name;
   String? subtitle;
   String? price;
 
-  
+  String? year;
+  String? like;
 
-
+  Doctor({this.profileImg, this.name, this.subtitle, this.price, this.year, this.like});
 }
