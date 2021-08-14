b90eebf55bf246c56b73b1b1e40d1ab9a90313cb
Dreamwalker <aristojeff@gmail.com>
2021-08-14 20:07:23 +0900

    Update doctor.dart

--- lib/ep877_doctor_appointment_app/model/doctor.dart
+++ lib/ep877_doctor_appointment_app/model/doctor.dart
@@ -9,3 +9,13 @@ class Doctor {
 
   Doctor({this.profileImg, this.name, this.subtitle, this.price, this.year, this.like});
 }
+
+List<Doctor> doctorItems = [
+  Doctor(
+      profileImg: "https://cdn.pixabay.com/photo/2017/01/29/21/16/nurse-2019420_960_720.jpg",
+      name: "Darrel Steward",
+      subtitle: "General Practitioner",
+      year: "3",
+      like: "92",
+      price: "80")
+];
