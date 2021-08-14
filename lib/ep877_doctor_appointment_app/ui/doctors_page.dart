539bfdd432b468a63681be609423dc83eb0156bc
Dreamwalker <aristojeff@gmail.com>
2021-08-14 20:22:50 +0900

    Update doctors_page.dart

--- lib/ep877_doctor_appointment_app/ui/doctors_page.dart
+++ lib/ep877_doctor_appointment_app/ui/doctors_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_14th_story/ep877_doctor_appointment_app/model/doctor.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
 
 class DoctorsPage extends StatefulWidget {
   const DoctorsPage({Key? key}) : super(key: key);
@@ -91,18 +92,31 @@ class _DoctorsPageState extends State<DoctorsPage> {
                                 radius: 32,
                                 backgroundImage: NetworkImage(doctorItems[index].profileImg ?? ""),
                               ),
+                              SizedBox(
+                                width: 16,
+                              ),
                               Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  Text(doctorItems[index].name ?? "Dreamwalker"),
-                                  Text(doctorItems[index].subtitle ?? "Dreamwalker"),
+                                  Text(
+                                    doctorItems[index].name ?? "Dreamwalker",
+                                    style: TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(vertical: 8),
+                                    child: Text(doctorItems[index].subtitle ?? "Dreamwalker",
+                                      style: TextStyle(fontSize: 12, color: Colors.blueGrey),),
+                                  ),
                                   Row(
                                     children: [
                                       CircleAvatar(
                                         radius: 10,
                                       ),
                                       SizedBox(width: 4),
-                                      Text("${doctorItems[index].year} years"),
+                                      Text(
+                                        "${doctorItems[index].year} years",
+                                        style: TextStyle(fontSize: 12, color: Colors.blueGrey),
+                                      ),
                                       SizedBox(
                                         width: 8,
                                       ),
@@ -112,7 +126,10 @@ class _DoctorsPageState extends State<DoctorsPage> {
                                       SizedBox(
                                         width: 4,
                                       ),
-                                      Text("${doctorItems[index].like} %"),
+                                      Text(
+                                        "${doctorItems[index].like} %",
+                                        style: TextStyle(fontSize: 12, color: Colors.blueGrey),
+                                      ),
                                     ],
                                   )
                                 ],
@@ -125,11 +142,30 @@ class _DoctorsPageState extends State<DoctorsPage> {
                           Row(
                             children: [
                               Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
                                   Text("Total fee"),
-                                  Text("\$${doctorItems[index].price}"),
+                                  Text(
+                                    "\$${doctorItems[index].price}",
+                                    style: TextStyle(fontWeight: FontWeight.bold),
+                                  ),
                                 ],
-                              )
+                              ),
+                              SizedBox(
+                                width: 16,
+                              ),
+                              Expanded(
+                                  child: Container(
+                                decoration:
+                                    BoxDecoration(color: Colors.orange, borderRadius: BorderRadius.circular(24)),
+                                padding: EdgeInsets.symmetric(vertical: 12),
+                                child: Center(
+                                  child: Text(
+                                    "Make an appointment",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              )),
                             ],
                           )
                         ],
