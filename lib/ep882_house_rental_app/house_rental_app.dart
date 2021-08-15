3435c8d5303d3da5702702b95c59158cbe4527e0
Dreamwalker <aristojeff@gmail.com>
2021-08-15 16:53:13 +0900

    Update house_rental_app.dart

--- lib/ep882_house_rental_app/house_rental_app.dart
+++ lib/ep882_house_rental_app/house_rental_app.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'ui/house_rental_home_page.dart';
+
 class HouseRentalApp extends StatelessWidget {
   const HouseRentalApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      routes: {
+        "/" : (context) => HouseRentalHomePage()
+      },
     );
   }
 }
