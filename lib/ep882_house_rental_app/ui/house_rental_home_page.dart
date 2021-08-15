0b417ac5582a4d6599c0985ad8af02b9ff541114
Dreamwalker <aristojeff@gmail.com>
2021-08-15 17:00:18 +0900

    Update house_rental_home_page.dart

--- lib/ep882_house_rental_app/ui/house_rental_home_page.dart
+++ lib/ep882_house_rental_app/ui/house_rental_home_page.dart
@@ -21,42 +21,74 @@ class _HouseRentalHomePageState extends State<HouseRentalHomePage> with SingleTi
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(child: Column(
-              children: [
-                Row(
-                  children: [
-                    Column(
-                      children: [
-                        Row(
-                          children: [
-                            Icon(Icons.location_on),
-                            Text("Location")
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Text("")
-                          ],
-                        )
-                      ],
-                    )
-                  ],
-                )
-              ],
-            ), flex: 6,),
-            TabBar(
-              tabs: [
-                Tab(text: "Recommended",),
-                Tab(text: "Nearest",),
-                Tab(text: "Popular",),
-              ],
-              controller: _tabController,
-            ),
-            Expanded(child: Placeholder(),
-            flex: 15,)
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.location_on,
+                                    size: 16,
+                                    color: Colors.blue,
+                                  ),
+                                  Text("Location")
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Text("Vienna, Austria"),
+                                  GestureDetector(
+                                      onTap: () {
+                                        showDialog(
+                                            context: context,
+                                            builder: (context) => AlertDialog(
+                                                  content: Text("Vienna, Austria"),
+                                                ));
+                                      },
+                                      child: Icon(Icons.keyboard_arrow_down))
+                                ],
+                              )
+                            ],
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                flex: 6,
+              ),
+              TabBar(
+                tabs: [
+                  Tab(
+                    text: "Recommended",
+                  ),
+                  Tab(
+                    text: "Nearest",
+                  ),
+                  Tab(
+                    text: "Popular",
+                  ),
+                ],
+                controller: _tabController,
+              ),
+              Expanded(
+                child: Placeholder(),
+                flex: 15,
+              )
+            ],
+          ),
         ),
       ),
     );
