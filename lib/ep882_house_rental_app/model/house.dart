6851a75571d86715d560ec7c27c06d21400b66a0
Dreamwalker <aristojeff@gmail.com>
2021-08-16 18:26:23 +0900

    Update house.dart

--- lib/ep882_house_rental_app/model/house.dart
+++ lib/ep882_house_rental_app/model/house.dart
@@ -1,11 +1,16 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
-part 'house.g.dart';
 part 'house.freezed.dart';
 
 @freezed
 class House with _$House {
   factory House({
     String? img,
+    String? star,
+    String? title,
+    String? pricePerNight,
+    String? location,
+    String? rooms,
+    String? area,
   }) = _House;
 }
