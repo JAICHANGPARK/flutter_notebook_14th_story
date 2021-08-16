d7f944b79662fc81b40d158620f3899a8122c144
Dreamwalker <aristojeff@gmail.com>
2021-08-16 18:27:14 +0900

    Create house.freezed.dart

---
+++ lib/ep882_house_rental_app/model/house.freezed.dart
@@ -0,0 +1,287 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'house.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$HouseTearOff {
+  const _$HouseTearOff();
+
+  _House call(
+      {String? img,
+      String? star,
+      String? title,
+      String? pricePerNight,
+      String? location,
+      String? rooms,
+      String? area}) {
+    return _House(
+      img: img,
+      star: star,
+      title: title,
+      pricePerNight: pricePerNight,
+      location: location,
+      rooms: rooms,
+      area: area,
+    );
+  }
+}
+
+/// @nodoc
+const $House = _$HouseTearOff();
+
+/// @nodoc
+mixin _$House {
+  String? get img => throw _privateConstructorUsedError;
+  String? get star => throw _privateConstructorUsedError;
+  String? get title => throw _privateConstructorUsedError;
+  String? get pricePerNight => throw _privateConstructorUsedError;
+  String? get location => throw _privateConstructorUsedError;
+  String? get rooms => throw _privateConstructorUsedError;
+  String? get area => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $HouseCopyWith<House> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $HouseCopyWith<$Res> {
+  factory $HouseCopyWith(House value, $Res Function(House) then) =
+      _$HouseCopyWithImpl<$Res>;
+  $Res call(
+      {String? img,
+      String? star,
+      String? title,
+      String? pricePerNight,
+      String? location,
+      String? rooms,
+      String? area});
+}
+
+/// @nodoc
+class _$HouseCopyWithImpl<$Res> implements $HouseCopyWith<$Res> {
+  _$HouseCopyWithImpl(this._value, this._then);
+
+  final House _value;
+  // ignore: unused_field
+  final $Res Function(House) _then;
+
+  @override
+  $Res call({
+    Object? img = freezed,
+    Object? star = freezed,
+    Object? title = freezed,
+    Object? pricePerNight = freezed,
+    Object? location = freezed,
+    Object? rooms = freezed,
+    Object? area = freezed,
+  }) {
+    return _then(_value.copyWith(
+      img: img == freezed
+          ? _value.img
+          : img // ignore: cast_nullable_to_non_nullable
+              as String?,
+      star: star == freezed
+          ? _value.star
+          : star // ignore: cast_nullable_to_non_nullable
+              as String?,
+      title: title == freezed
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String?,
+      pricePerNight: pricePerNight == freezed
+          ? _value.pricePerNight
+          : pricePerNight // ignore: cast_nullable_to_non_nullable
+              as String?,
+      location: location == freezed
+          ? _value.location
+          : location // ignore: cast_nullable_to_non_nullable
+              as String?,
+      rooms: rooms == freezed
+          ? _value.rooms
+          : rooms // ignore: cast_nullable_to_non_nullable
+              as String?,
+      area: area == freezed
+          ? _value.area
+          : area // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$HouseCopyWith<$Res> implements $HouseCopyWith<$Res> {
+  factory _$HouseCopyWith(_House value, $Res Function(_House) then) =
+      __$HouseCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {String? img,
+      String? star,
+      String? title,
+      String? pricePerNight,
+      String? location,
+      String? rooms,
+      String? area});
+}
+
+/// @nodoc
+class __$HouseCopyWithImpl<$Res> extends _$HouseCopyWithImpl<$Res>
+    implements _$HouseCopyWith<$Res> {
+  __$HouseCopyWithImpl(_House _value, $Res Function(_House) _then)
+      : super(_value, (v) => _then(v as _House));
+
+  @override
+  _House get _value => super._value as _House;
+
+  @override
+  $Res call({
+    Object? img = freezed,
+    Object? star = freezed,
+    Object? title = freezed,
+    Object? pricePerNight = freezed,
+    Object? location = freezed,
+    Object? rooms = freezed,
+    Object? area = freezed,
+  }) {
+    return _then(_House(
+      img: img == freezed
+          ? _value.img
+          : img // ignore: cast_nullable_to_non_nullable
+              as String?,
+      star: star == freezed
+          ? _value.star
+          : star // ignore: cast_nullable_to_non_nullable
+              as String?,
+      title: title == freezed
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String?,
+      pricePerNight: pricePerNight == freezed
+          ? _value.pricePerNight
+          : pricePerNight // ignore: cast_nullable_to_non_nullable
+              as String?,
+      location: location == freezed
+          ? _value.location
+          : location // ignore: cast_nullable_to_non_nullable
+              as String?,
+      rooms: rooms == freezed
+          ? _value.rooms
+          : rooms // ignore: cast_nullable_to_non_nullable
+              as String?,
+      area: area == freezed
+          ? _value.area
+          : area // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_House implements _House {
+  _$_House(
+      {this.img,
+      this.star,
+      this.title,
+      this.pricePerNight,
+      this.location,
+      this.rooms,
+      this.area});
+
+  @override
+  final String? img;
+  @override
+  final String? star;
+  @override
+  final String? title;
+  @override
+  final String? pricePerNight;
+  @override
+  final String? location;
+  @override
+  final String? rooms;
+  @override
+  final String? area;
+
+  @override
+  String toString() {
+    return 'House(img: $img, star: $star, title: $title, pricePerNight: $pricePerNight, location: $location, rooms: $rooms, area: $area)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _House &&
+            (identical(other.img, img) ||
+                const DeepCollectionEquality().equals(other.img, img)) &&
+            (identical(other.star, star) ||
+                const DeepCollectionEquality().equals(other.star, star)) &&
+            (identical(other.title, title) ||
+                const DeepCollectionEquality().equals(other.title, title)) &&
+            (identical(other.pricePerNight, pricePerNight) ||
+                const DeepCollectionEquality()
+                    .equals(other.pricePerNight, pricePerNight)) &&
+            (identical(other.location, location) ||
+                const DeepCollectionEquality()
+                    .equals(other.location, location)) &&
+            (identical(other.rooms, rooms) ||
+                const DeepCollectionEquality().equals(other.rooms, rooms)) &&
+            (identical(other.area, area) ||
+                const DeepCollectionEquality().equals(other.area, area)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(img) ^
+      const DeepCollectionEquality().hash(star) ^
+      const DeepCollectionEquality().hash(title) ^
+      const DeepCollectionEquality().hash(pricePerNight) ^
+      const DeepCollectionEquality().hash(location) ^
+      const DeepCollectionEquality().hash(rooms) ^
+      const DeepCollectionEquality().hash(area);
+
+  @JsonKey(ignore: true)
+  @override
+  _$HouseCopyWith<_House> get copyWith =>
+      __$HouseCopyWithImpl<_House>(this, _$identity);
+}
+
+abstract class _House implements House {
+  factory _House(
+      {String? img,
+      String? star,
+      String? title,
+      String? pricePerNight,
+      String? location,
+      String? rooms,
+      String? area}) = _$_House;
+
+  @override
+  String? get img => throw _privateConstructorUsedError;
+  @override
+  String? get star => throw _privateConstructorUsedError;
+  @override
+  String? get title => throw _privateConstructorUsedError;
+  @override
+  String? get pricePerNight => throw _privateConstructorUsedError;
+  @override
+  String? get location => throw _privateConstructorUsedError;
+  @override
+  String? get rooms => throw _privateConstructorUsedError;
+  @override
+  String? get area => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$HouseCopyWith<_House> get copyWith => throw _privateConstructorUsedError;
+}
