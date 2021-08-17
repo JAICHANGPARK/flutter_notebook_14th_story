66955b7bf894a5f0ba0a9aa5fbd492a500fcdca0
Dreamwalker <aristojeff@gmail.com>
2021-08-17 19:53:17 +0900

    Update house.dart

--- lib/ep882_house_rental_app/model/house.dart
+++ lib/ep882_house_rental_app/model/house.dart
@@ -15,5 +15,30 @@ class House with _$House {
   }) = _House;
 }
 
+List<House> houseItems = [
+  House(
+      title: "City Night House",
+      area: "250 ms",
+      rooms: "5 Rooms",
+      location: "Bowler Str. N19",
+      pricePerNight: "\$450",
+      star: "5.0",
+      img: "https://cdn.pixabay.com/photo/2014/07/10/17/18/large-home-389271_960_720.jpg"),
+  House(
+      title: "City Night House",
+      area: "250 ms",
+      rooms: "5 Rooms",
+      location: "Bowler Str. N19",
+      pricePerNight: "\$450",
+      star: "5.0",
+      img: "https://cdn.pixabay.com/photo/2014/07/10/17/18/large-home-389271_960_720.jpg"),
+  House(
+      title: "City Night House",
+      area: "250 ms",
+      rooms: "5 Rooms",
+      location: "Bowler Str. N19",
+      pricePerNight: "\$450",
+      star: "5.0",
+      img: "https://cdn.pixabay.com/photo/2014/07/10/17/18/large-home-389271_960_720.jpg"),
 
-List<House> houseItems = [];
+];
