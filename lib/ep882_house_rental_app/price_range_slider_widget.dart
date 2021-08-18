c27a338dd9bdf2296d908542ccaa5f53c17b0c53
Dreamwalker <aristojeff@gmail.com>
2021-08-18 20:18:17 +0900

    Create price_range_slider_widget.dart

---
+++ lib/ep882_house_rental_app/price_range_slider_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_notebook_14th_story/ep882_house_rental_app/provider/house_filter_provider.dart';
+import 'package:provider/provider.dart';
+
+class PriceRangeSliderWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final hfProvider = Provider.of<HouseFilterProvider>(context);
+    return RangeSlider(
+      values: hfProvider.priceRangeValues,
+      onChanged: (rv) {},
+      min: 0,
+      max: 5000,
+    );
+  }
+}
