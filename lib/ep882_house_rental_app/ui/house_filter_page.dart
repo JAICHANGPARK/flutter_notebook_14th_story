ecc3b89a40f5daa943f85c6e23fe8d0b7bbdb5d4
Dreamwalker <aristojeff@gmail.com>
2021-08-18 19:55:57 +0900

    Create house_filter_page.dart

---
+++ lib/ep882_house_rental_app/ui/house_filter_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class HouseFilterPage extends StatelessWidget {
+  const HouseFilterPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
