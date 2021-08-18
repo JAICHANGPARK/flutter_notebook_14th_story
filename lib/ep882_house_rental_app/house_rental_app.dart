ddc1b9165bfd0bad0464be06ea5915f421eb8624
Dreamwalker <aristojeff@gmail.com>
2021-08-18 20:03:12 +0900

    Update house_rental_app.dart

--- lib/ep882_house_rental_app/house_rental_app.dart
+++ lib/ep882_house_rental_app/house_rental_app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_14th_story/ep882_house_rental_app/provider/house_filter_provider.dart';
+import 'package:provider/provider.dart';
 
 import 'ui/house_filter_page.dart';
 import 'ui/house_rental_home_page.dart';
@@ -8,11 +10,14 @@ class HouseRentalApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        "/" : (context) => HouseRentalHomePage(),
-        "/search/filter" : (context) => HouseFilterPage()
-      },
+    return ChangeNotifierProvider(
+      create: (BuildContext context)  => HouseFilterProvider(),
+      child: MaterialApp(
+        routes: {
+          "/" : (context) => HouseRentalHomePage(),
+          "/search/filter" : (context) => HouseFilterPage()
+        },
+      ),
     );
   }
 }
