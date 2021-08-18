57c5ecb8ecff9f9e507b9455bf542e3a8bcca68a
Dreamwalker <aristojeff@gmail.com>
2021-08-18 20:18:19 +0900

    Update house_filter_provider.dart

--- lib/ep882_house_rental_app/provider/house_filter_provider.dart
+++ lib/ep882_house_rental_app/provider/house_filter_provider.dart
@@ -1,11 +1,19 @@
-
-
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
-class HouseFilterProvider extends ChangeNotifier{
+class HouseFilterProvider extends ChangeNotifier {
   double _priceMin = 0.0;
   double _priceMax = 0.0;
 
   double get priceMin => _priceMin;
+
   double get priceMax => _priceMax;
+
+  HouseFilterProvider() {
+    _priceRangeValues = RangeValues(_priceMin, _priceMax);
+  }
+
+  late RangeValues _priceRangeValues;
+
+  RangeValues get priceRangeValues => _priceRangeValues;
 }
