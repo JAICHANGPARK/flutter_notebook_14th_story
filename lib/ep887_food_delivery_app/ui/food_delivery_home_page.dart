4dac89d7a217fa3225146033973d2eeb405f5616
Dreamwalker <aristojeff@gmail.com>
2021-08-20 19:37:57 +0900

    Create food_delivery_home_page.dart

---
+++ lib/ep887_food_delivery_app/ui/food_delivery_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FoodDeliveryHomePage extends StatefulWidget {
+  const FoodDeliveryHomePage({Key? key}) : super(key: key);
+
+  @override
+  _FoodDeliveryHomePageState createState() => _FoodDeliveryHomePageState();
+}
+
+class _FoodDeliveryHomePageState extends State<FoodDeliveryHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
