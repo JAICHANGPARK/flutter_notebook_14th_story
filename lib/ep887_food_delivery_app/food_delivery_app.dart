ee3b06818805f93c968d15331ad5c1302720a885
Dreamwalker <aristojeff@gmail.com>
2021-08-20 19:36:26 +0900

    Update food_delivery_app.dart

---
+++ lib/ep887_food_delivery_app/food_delivery_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class FoodDeliveryApp extends StatelessWidget {
+  const FoodDeliveryApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
