f89c3ce3fe924d953905edd9ead942697ad5d559
Dreamwalker <aristojeff@gmail.com>
2021-08-20 19:39:06 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'ep882_house_rental_app/house_rental_app.dart';
+import 'ep887_food_delivery_app/food_delivery_app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(HouseRentalApp());
+  runApp(FoodDeliveryApp());
 }
