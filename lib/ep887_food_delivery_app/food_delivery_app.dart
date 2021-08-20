c86aa933ced539ad368b89677f35b814ae544f8e
Dreamwalker <aristojeff@gmail.com>
2021-08-20 19:37:22 +0900

    Update food_delivery_app.dart

--- lib/ep887_food_delivery_app/food_delivery_app.dart
+++ lib/ep887_food_delivery_app/food_delivery_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 
 class FoodDeliveryApp extends StatelessWidget {
@@ -6,6 +7,8 @@ class FoodDeliveryApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GetMaterialApp(
+
+    );
   }
 }
