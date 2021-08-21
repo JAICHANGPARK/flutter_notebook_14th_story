67cb7c1f5e6d095848bc6111aff38d616daec851
Dreamwalker <aristojeff@gmail.com>
2021-08-21 20:39:27 +0900

    Update fd_nutrition_page.dart

---
+++ lib/ep887_food_delivery_app/ui/fd_nutrition_page.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+
+class FdNutritionPage extends StatelessWidget {
+  const FdNutritionPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('FdNutritionPage'),
+    );
+  }
+}
