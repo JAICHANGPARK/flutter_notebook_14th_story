74fa97e500f7af10a3275a1e4d106f19f0742469
Dreamwalker <aristojeff@gmail.com>
2021-08-21 20:31:14 +0900

    Update fd_order_page.dart

---
+++ lib/ep887_food_delivery_app/ui/fd_order_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FdOrderPage extends StatefulWidget {
+  const FdOrderPage({Key? key}) : super(key: key);
+
+  @override
+  _FdOrderPageState createState() => _FdOrderPageState();
+}
+
+class _FdOrderPageState extends State<FdOrderPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
