9a2917295916147cd1cc3a5a73f8e98deb6c38ec
Dreamwalker <aristojeff@gmail.com>
2021-08-21 20:43:49 +0900

    Update fd_order_page.dart

--- lib/ep887_food_delivery_app/ui/fd_order_page.dart
+++ lib/ep887_food_delivery_app/ui/fd_order_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class FdOrderPage extends StatefulWidget {
   const FdOrderPage({Key? key}) : super(key: key);
 
@@ -13,8 +12,27 @@ class _FdOrderPageState extends State<FdOrderPage> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Expanded(child: Placeholder()),
-        Expanded(child: Placeholder()),
+        Expanded(
+            flex: 2,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text("Home"),
+                        Icon(Icons.keyboard_arrow_down),
+                      ],
+                    ),
+                    Text("South Korea, Seoul"),
+                  ],
+                )
+              ],
+            )),
+        Expanded(flex: 15, child: Placeholder()),
       ],
     );
   }
