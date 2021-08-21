08e9174260643b089fdc6ccb2f6339fcf7955ad7
Dreamwalker <aristojeff@gmail.com>
2021-08-21 20:35:23 +0900

    Update fd_go_out_page.dart

---
+++ lib/ep887_food_delivery_app/ui/fd_go_out_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class FdGoOutPage extends StatelessWidget {
+  const FdGoOutPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Go out page'),
+    );
+  }
+}
