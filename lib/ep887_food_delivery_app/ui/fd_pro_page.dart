6e47b1d599d7bbb00311535584f08a68cbb1af8b
Dreamwalker <aristojeff@gmail.com>
2021-08-21 20:37:52 +0900

    Update fd_pro_page.dart

---
+++ lib/ep887_food_delivery_app/ui/fd_pro_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class FdProPage extends StatelessWidget {
+  const FdProPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Pro Page'),
+    );
+  }
+}
