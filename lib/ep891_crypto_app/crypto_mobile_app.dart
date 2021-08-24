bc8bb20a01d15cca67abfec9d3c68248c30df93b
Dreamwalker <aristojeff@gmail.com>
2021-08-24 19:27:14 +0900

    Update crypto_mobile_app.dart

--- lib/ep891_crypto_app/crypto_mobile_app.dart
+++ lib/ep891_crypto_app/crypto_mobile_app.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'ui/crypto_mobile_main_page.dart';
 
 class CryptoMobileApp extends StatelessWidget {
-  const CryptoMobileApp({Key? key}) : super(key: key);
+  CryptoMobileApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
