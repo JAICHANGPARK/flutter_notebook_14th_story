652470bebfddbe74833de40d215288aefe208576
Dreamwalker <aristojeff@gmail.com>
2021-08-26 19:10:30 +0900

    Create my_assets.dart

---
+++ lib/ep891_crypto_app/model/my_assets.dart
@@ -0,0 +1,20 @@
+String kCryptoMyAsset = '''
+    {
+      "balance" : "23,874.45",
+      "this_week" : "6.78",
+      "my_assets" : [
+        {
+            "coin" : "Bitcoin",
+            "unit" : "BTC",
+            "price": "1,953.45",
+            "change" :{
+              "up_down" : "up",
+              "rate" : "3.05"
+            }
+            
+        }
+      ]
+    }
+    ''';
+
+
