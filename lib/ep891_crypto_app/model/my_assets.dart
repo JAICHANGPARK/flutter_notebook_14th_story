c82d1d93fad555aaff9b904c84834dc863e10346
Dreamwalker <aristojeff@gmail.com>
2021-08-26 19:13:00 +0900

    Update my_assets.dart

--- lib/ep891_crypto_app/model/my_assets.dart
+++ lib/ep891_crypto_app/model/my_assets.dart
@@ -1,29 +1,77 @@
 String kCryptoMyAsset = '''
-    {
-      "balance" : "23,874.45",
-      "this_week" : "6.78",
-      "my_assets" : [
+{
+  "balance" : "23,874.45",
+  "this_week" : "6.78",
+  "my_assets" : [
         {
             "coin" : "Bitcoin",
             "unit" : "BTC",
             "price": "1,953.45",
-            "change" :{
+            "change" : {
               "up_down" : "up",
               "rate" : "3.05"
-            }
-            
         }
-      ]
+       
     }
-    ''';
+  ]
+}
+''';
 
 class MyAsset {
-    String balance;
-    String thisWeek;
-    List<MyAssets> myAssets;
-    MyAsset({this.balance, this.thisWeek, this.myAssets});
+  String? balance;
+  String? thisWeek;
+  List<MyAssets>? myAssets;
 
-    
+  MyAsset({this.balance, this.thisWeek, this.myAssets});
 
-
+  MyAsset.fromJson(Map<String, dynamic> json) {}
 }
+
+class MyAssets {
+    String coin;
+    String unit;
+    String price;
+    Change change;
+
+    MyAssets({this.coin, this.unit, this.price, this.change});
+
+    MyAssets.fromJson(Map<String, dynamic> json) {
+        coin = json['coin'];
+        unit = json['unit'];
+        price = json['price'];
+        change =
+        json['change'] != null ? new Change.fromJson(json['change']) : null;
+    }
+
+    Map<String, dynamic> toJson() {
+        final Map<String, dynamic> data = new Map<String, dynamic>();
+        data['coin'] = this.coin;
+        data['unit'] = this.unit;
+        data['price'] = this.price;
+        if (this.change != null) {
+            data['change'] = this.change.toJson();
+        }
+        return data;
+    }
+}
+
+class Change {
+    String upDown;
+    String rate;
+
+    Change({this.upDown, this.rate});
+
+    Change.fromJson(Map<String, dynamic> json) {
+        upDown = json['up_down'];
+        rate = json['rate'];
+    }
+
+    Map<String, dynamic> toJson() {
+        final Map<String, dynamic> data = new Map<String, dynamic>();
+        data['up_down'] = this.upDown;
+        data['rate'] = this.rate;
+        return data;
+    }
+}
+
+
