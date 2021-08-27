69c4b31ff4780730e4f222f6b4db190dbcd5a188
Dreamwalker <aristojeff@gmail.com>
2021-08-27 19:18:17 +0900

    Update crypto_asset.dart

---
+++ lib/ep891_crypto_app/model/crypto_asset.dart
@@ -0,0 +1,44 @@
+String kCryptoAsset = '''
+{
+  "crypto_assets" : [
+        {
+            "coin" : "Doge Coin",
+            "unit" : "DOGE",
+            "price": "26.34",
+            "change" : {
+              "up_down" : "down",
+              "rate" : "0.23"
+              }
+        },
+        {
+            "coin" : "Bitcoin",
+            "unit" : "BTC",
+            "price": "1,953.45",
+            "change" : {
+              "up_down" : "up",
+              "rate" : "3.05"
+              }
+        },
+        {
+            "coin" : "Ethereum",
+            "unit" : "ETH",
+            "price": "893.65",
+            "change" : {
+              "up_down" : "up",
+              "rate" : "3.64"
+              }
+        },
+        {
+            "coin" : "Binance Coin",
+            "unit" : "BNB",
+            "price": "484.10",
+            "change" : {
+              "up_down" : "down",
+              "rate" : "4.56"
+              }
+        }
+  ]
+}
+''';
+
+
