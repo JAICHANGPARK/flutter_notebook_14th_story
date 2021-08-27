4446233eb90292673ddba5265c5eb2e18efffdae
Dreamwalker <aristojeff@gmail.com>
2021-08-27 19:19:42 +0900

    Update crypto_asset.dart

--- lib/ep891_crypto_app/model/crypto_asset.dart
+++ lib/ep891_crypto_app/model/crypto_asset.dart
@@ -1,3 +1,5 @@
+import 'my_assets.dart';
+
 String kCryptoAsset = '''
 {
   "crypto_assets" : [
@@ -41,4 +43,52 @@ String kCryptoAsset = '''
 }
 ''';
 
+class CryptoAsset {
+  List<CryptoAssets>? cryptoAssets;
 
+  CryptoAsset({this.cryptoAssets});
+
+  CryptoAsset.fromJson(Map<String, dynamic> json) {
+    if (json['crypto_assets'] != null) {
+      cryptoAssets = [];
+      json['crypto_assets'].forEach((v) {
+        cryptoAssets?.add(CryptoAssets.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = {};
+    if (cryptoAssets != null) {
+      data['crypto_assets'] = cryptoAssets.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class CryptoAssets {
+  String coin;
+  String unit;
+  String price;
+  Change change;
+
+  CryptoAssets({this.coin, this.unit, this.price, this.change});
+
+  CryptoAssets.fromJson(Map<String, dynamic> json) {
+    coin = json['coin'];
+    unit = json['unit'];
+    price = json['price'];
+    change = json['change'] != null ? new Change.fromJson(json['change']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['coin'] = this.coin;
+    data['unit'] = this.unit;
+    data['price'] = this.price;
+    if (this.change != null) {
+      data['change'] = this.change.toJson();
+    }
+    return data;
+  }
+}
