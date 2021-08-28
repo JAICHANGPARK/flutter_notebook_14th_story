8868a9dfd15aaf4bcecd57b523c409835325b411
Dreamwalker <aristojeff@gmail.com>
2021-08-28 19:56:26 +0900

    Update crypto_portofolio_page.dart

--- lib/ep891_crypto_app/ui/crypto_portofolio_page.dart
+++ lib/ep891_crypto_app/ui/crypto_portofolio_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-
-
 class CryptoPortfolioPage extends StatefulWidget {
   CryptoPortfolioPage({Key? key}) : super(key: key);
 
@@ -15,7 +13,21 @@ class _CryptoPortfolioPageState extends State<CryptoPortfolioPage> {
     return SingleChildScrollView(
       child: Column(
         children: [
-
+          Text("Portofolio"),
+          Row(
+            children: [
+              Column(
+                children: [Text("\$23,874.45"), Text("Portofolio balance")],
+              ),
+              FloatingActionButton(
+                onPressed: () {},
+                child: Icon(Icons.arrow_forward),
+                backgroundColor: Colors.white,
+                elevation: 3,
+                foregroundColor: Colors.black,
+              )
+            ],
+          )
         ],
       ),
     );
