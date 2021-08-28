2324848c1735bdf9cb53b40dd1dd861da35bafef
Dreamwalker <aristojeff@gmail.com>
2021-08-28 19:48:43 +0900

    Update crypto_portofolio_page.dart

---
+++ lib/ep891_crypto_app/ui/crypto_portofolio_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+
+
+class CryptoPortfolioPage extends StatefulWidget {
+  const CryptoPortfolioPage({Key? key}) : super(key: key);
+
+  @override
+  _CryptoPortfolioPageState createState() => _CryptoPortfolioPageState();
+}
+
+class _CryptoPortfolioPageState extends State<CryptoPortfolioPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
