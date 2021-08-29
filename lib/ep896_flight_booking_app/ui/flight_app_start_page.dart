23630d10e85fcc8703005e3879f1a5d9ce1a7769
Dreamwalker <aristojeff@gmail.com>
2021-08-29 18:49:49 +0900

    Update flight_app_start_page.dart

--- lib/ep896_flight_booking_app/ui/flight_app_start_page.dart
+++ lib/ep896_flight_booking_app/ui/flight_app_start_page.dart
@@ -7,7 +7,27 @@ class FlightAppStartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+                left: 16,
+                top: 84,
+                right: 0,
 
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Easily book",style: TextStyle(
+                  fontSize: 32
+                ),),
+                Text("Flight")
+              ],
+            )),
+
+          ],
+        ),
+      ),
     );
   }
 }
