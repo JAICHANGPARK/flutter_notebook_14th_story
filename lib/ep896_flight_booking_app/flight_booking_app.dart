fc21512b6616f9813b33035e6c92ff2a5191bc08
Dreamwalker <aristojeff@gmail.com>
2021-08-29 18:43:20 +0900

    Create flight_booking_app.dart

---
+++ lib/ep896_flight_booking_app/flight_booking_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+
+class FlightBookingApp extends StatelessWidget {
+  const FlightBookingApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      
+    );
+  }
+}
