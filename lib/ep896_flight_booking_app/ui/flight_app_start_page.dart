73d06f96f13e59aedfd4893d27cc1931bc168ecb
Dreamwalker <aristojeff@gmail.com>
2021-08-29 18:44:16 +0900

    Create flight_app_start_page.dart

---
+++ lib/ep896_flight_booking_app/ui/flight_app_start_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class FlightAppStartPage extends StatelessWidget {
+  const FlightAppStartPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
