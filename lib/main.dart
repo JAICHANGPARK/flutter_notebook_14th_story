1dfc1e75590cc14f35f8687a515b3d3d82dc49f6
Dreamwalker <aristojeff@gmail.com>
2021-08-29 18:46:08 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'ep891_crypto_app/crypto_mobile_app.dart';
+import 'ep896_flight_booking_app/flight_booking_app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(CryptoMobileApp());
+  runApp(FlightBookingApp());
 }
