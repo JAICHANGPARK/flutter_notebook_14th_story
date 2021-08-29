5255c41413bca9c5ffb3c567caf191747bf8e3a0
Dreamwalker <aristojeff@gmail.com>
2021-08-29 18:46:05 +0900

    Update flight_booking_app.dart

--- lib/ep896_flight_booking_app/flight_booking_app.dart
+++ lib/ep896_flight_booking_app/flight_booking_app.dart
@@ -11,6 +11,7 @@ class FlightBookingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      initialRoute: "/",
       routes: {
         "/" : (context) =>FlightAppStartPage(),
         "/ticket" : (context) => FlightTicketPage()
