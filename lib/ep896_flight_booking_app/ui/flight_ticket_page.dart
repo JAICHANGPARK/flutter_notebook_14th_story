fe377669a831857a3316120405ba383fe98b1df0
Dreamwalker <aristojeff@gmail.com>
2021-08-29 18:45:41 +0900

    Create flight_ticket_page.dart

---
+++ lib/ep896_flight_booking_app/ui/flight_ticket_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FlightTicketPage extends StatefulWidget {
+  const FlightTicketPage({Key? key}) : super(key: key);
+
+  @override
+  _FlightTicketPageState createState() => _FlightTicketPageState();
+}
+
+class _FlightTicketPageState extends State<FlightTicketPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
