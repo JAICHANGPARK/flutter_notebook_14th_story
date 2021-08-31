304465fe60dae7a75f269a2a176e44366a8bdbd4
Dreamwalker <aristojeff@gmail.com>
2021-08-31 19:19:45 +0900

    Update flight_ticket.dart

--- lib/ep896_flight_booking_app/model/flight_ticket.dart
+++ lib/ep896_flight_booking_app/model/flight_ticket.dart
@@ -1,12 +1,11 @@
-
-
-class FlightTicket{
-    String? title;
-    String? fromDate;
-    String? fromTime;
-    String? arriveDate;
-    String? arriveTime;
-    String? origin;
-    String? destination;
-
+class FlightTicket {
+  String? title;
+  String? fromDate;
+  String? fromTime;
+  String? arriveDate;
+  String? arriveTime;
+  String? origin;
+  String? destination;
+  String? elapsedTime;
+  bool? isRunning;
 }
