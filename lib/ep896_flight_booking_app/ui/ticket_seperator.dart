ed8ee981aead8136996ccc5593c6ff5a677c8cc4
Dreamwalker <aristojeff@gmail.com>
2021-08-31 19:37:25 +0900

    Update ticket_seperator.dart

--- lib/ep896_flight_booking_app/ui/ticket_seperator.dart
+++ lib/ep896_flight_booking_app/ui/ticket_seperator.dart
@@ -1,3 +1,24 @@
-
 import 'package:flutter/material.dart';
 
+class TicketSeparator extends StatelessWidget {
+  final double height;
+  final Color color;
+
+  const TicketSeparator({Key? key, this.height = 1, this.color = Colors.black}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      final boxWidth = constraints.constrainWidth();
+      final dashWidth = 10.0;
+      return Flex(
+        direction: Axis.horizontal,
+        children: List.generate(
+            8,
+            (index) => SizedBox(
+                  height: dashWidth,
+                )),
+      );
+    });
+  }
+}
