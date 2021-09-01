962d191c6e0ea15d95395f502102dc39c438ecec
Dreamwalker <aristojeff@gmail.com>
2021-09-01 18:57:59 +0900

    Create qr_scan_page.dart

---
+++ lib/ep896_flight_booking_app/ui/qr_scan_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+class QrScanPage extends StatefulWidget {
+  const QrScanPage({Key? key}) : super(key: key);
+
+  @override
+  _QrScanPageState createState() => _QrScanPageState();
+}
+
+class _QrScanPageState extends State<QrScanPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
